45e32c3ecd53b38d51c300cf79b260e411df8b61
JOSHUA DODGE WOODWARD <jdw6359@condor.(none)>
2014-04-27 17:36:04 -0400

    Completed nak module

--- interp.c
+++ interp.c
@@ -48,7 +48,7 @@ extern void cspline_clamped(Points *points, double derivA, double derivB, CSplin
 	int row, col, j, startingCol, count;
 
 	/* Declare values that will be put into our linear system as well as C, C+1 */
-	double hValue, alpha, Cj, Cj1;
+	double hValue, alpha;
 
 	/* Make calls to allocate memory for matrices and vectors */
 	matrix=matrix_alloc(numRows, numRows);
@@ -228,8 +228,119 @@ extern void cspline_natural(Points *points, CSplines *spline){
 }
 
 /* Finds the coefficients of the not-a-knot cubic cpline for a given set of data */
-extern void cspline_nak(Points *data, CSplines *splines){
+extern void cspline_nak(Points *points, CSplines *spline){
 
+	/* Set number of rows to number of data points in the struct -2 */
+	/* If there are N+1 number of points, there are N number of splines */
+	int numRows=points->N-2;
+
+	/* N represente num points - 1 */
+	int N=points->N-1;
+
+	/* Declare matrix variables */
+	MatElement **matrix;
+
+	/* Declare right vector a nd solution variable */
+	VectorElement *right, *solution;
+
+	/* Declare values for counters */
+	int row, col, j, startingCol, count;
+
+	/* Declare values that will be put into our linear system as well as C, and C1 */
+	double hValue, alpha, Cj, Cj1;
+
+	/* Make calls to allocate memory for matruces and vectors */
+	matrix=matrix_alloc(numRows, numRows);
+	right=vector_alloc(numRows);
+	solution=vector_alloc(numRows);
+
+	for(row=0;row<numRows;row++){
+
+		/* The col that our values will start to be put into */
+		startingCol=row-1;
+
+		/* Count represents the basis for the formula that we will be using */
+		for(count=0;count<3;count++){
+
+			/* The col we are on is the sum of the column that we start at + count */
+			col=startingCol+count;
+
+			/* Check to make sure [row][col] is in bounds */
+			if((col>=0)&&(col<numRows)){
+
+				hValue=5;
+
+
+				if(count==0){
+					if(row+1==numRows){
+						hValue=(points->X[row+1] - points->X[row]) - (((points->X[row+2]-points->X[row+1])*(points->X[row+2]-points->X[row+1]))/(points->X[row+1]-points->X[row]));
+					}else{
+						hValue=points->X[row+1] - points->X[row];
+					}
+				}else if(count==1){
+					if(row==0){
+						hValue=(3*(points->X[row+1] - points->X[row])) + (2*(points->X[row+2] - points->X[row+1])) + (((points->X[row+1] - points->X[row])*(points->X[row+1] - points->X[row]))/(points->X[row+2] - points->X[row+1]));
+					}else if(row+1==numRows){
+						hValue=((3*(points->X[row+2]-points->X[row+1]))+(2*(points->X[row+1]-points->X[row]))+(((points->X[row+2]-points->X[row+1])*(points->X[row+2]-points->X[row+1]))/(points->X[row+1]-points->X[row])));
+					}else{
+						hValue=2*((points->X[row+1]-points->X[row])+(points->X[row+2] - points->X[row+1]));
+					}
+
+
+				}else if(count==2){
+					if(row==0){
+						hValue=(points->X[row+2] - points->X[row+1])-(((points->X[row+1] - points->X[row])*(points->X[row+1] - points->X[row]))/(points->X[row+2] - points->X[row+1]));
+					}else{
+						hValue=points->X[row+2] - points->X[row+1];
+					}
+
+
+				}
+
+				/* Set value */
+				matrix[row][col]=hValue;
+
+			}
+		}
+	}
+
+	/* Set "right" values */
+	for(row=0;row<numRows;row++){
+
+		/* j is the "sub" value to index into other structs */
+		j=row+1;
+
+		/* Compute and set value of alpha */
+
+		alpha=3*(((points->Y[j+1]-points->Y[j])/(points->X[j+1]-points->X[j]))-((points->Y[j]-points->Y[j-1])/(points->X[j]-points->X[j-1])));
+		right[row]=alpha;
+
+	}
+
+	/* make call to tridiagonal to solve for c values */
+	tridiagonal(points, spline, matrix, right, solution, numRows);
+
+
+	/* We now have c(1) -> C(n-1) */
+	for(j=0;j<N;j++){
+		if(j==0){
+			Cj=((1+((points->X[2]-points->X[2])/(points->X[1]-points->X[0])))*(solution[j]))-(((points->X[1]-points->X[0])/(points->X[2]-points->X[1]))*(solution[1]));
+			Cj1=solution[j];
+
+		}else{
+			Cj=solution[j-1];
+			if(j+1==N){
+				Cj1=(((points->X[j+1]-points->X[j])/(points->X[j]-points->X[j-1]))*(solution[j-2]))+((1+((points->X[j+1]-points->X[j])/(points->X[j]-points->X[j-1])))*(solution[j-1]));
+			}else{
+				Cj1=solution[j];
+			}
+		}
+
+		/* Make call to solve_coeff */
+		solve_coeff(points, spline, Cj, Cj1, j);
+	}
+
+	matrix_print(matrix, " %g ", numRows, numRows);
 }
 
 
@@ -252,10 +363,8 @@ static void tridiagonal(Points *points, CSplines *spline, MatElement **matrix, V
 	/* Create permutation matrix */
 	perm=matrix_identity(numRows);
 
-	/* Make call to linalg_LU_decomp */
 	linalg_LU_decomp(matrix, perm, numRows);
 
-	/* make call to linalg_LU_solve */
 	linalg_LU_solve(matrix, perm, right, solution, numRows);
 
 	print_plu(matrix, perm, numRows);
